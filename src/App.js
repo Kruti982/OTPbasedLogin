69bab2f4eab4feb2701b51e8f4c77d85b51419b1
kruti <kruti.dimpi@gmail.com>
2025-04-10 15:51:50 +0530

    OTP login

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import Routing from './components/Routing/Routing'
+function App() {
+  return (
+    <Routing/>
+import Routing from "./components/routing/Routing";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Routing />
+      </div>
+    </>
+  );
+}
+
+export default App;
